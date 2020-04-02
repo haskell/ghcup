21b6756512537a9226fdf71ce6fc4a9fb925232c
Herbert Valerio Riedel <hvr@gnu.org>
2020-04-02 10:46:43 +0200

    Update shellcheck-latest url
    see https://github.com/koalaman/shellcheck/issues/1871

--- .gitlab/before_script/linux/install_shellcheck.sh
+++ .gitlab/before_script/linux/install_shellcheck.sh
@@ -3,7 +3,7 @@
 set -eux
 
 # install shellcheck
-wget https://storage.googleapis.com/shellcheck/shellcheck-latest.linux.x86_64.tar.xz
+wget https://github.com/koalaman/shellcheck/releases/download/latest/shellcheck-latest.linux.x86_64.tar.xz
 tar -xJf shellcheck-latest.linux.x86_64.tar.xz
 mkdir -p "$CI_PROJECT_DIR"/.local/bin/
 mv shellcheck-latest/shellcheck "$CI_PROJECT_DIR"/.local/bin/shellcheck
